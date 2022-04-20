0e5e2aef6fbf6ab839895e8b31d8f48f3ef55132
Rodrigo <rsalomao@paypal.com>
2022-04-19 21:59:01 -0300

    Added navigation and first fragment empty

--- app/src/main/java/com/salomao/movies/presentation/MainActivity.kt
+++ app/src/main/java/com/salomao/movies/presentation/MainActivity.kt
@@ -1,7 +1,8 @@
-package com.salomao.movies
+package com.salomao.movies.presentation
 
 import androidx.appcompat.app.AppCompatActivity
 import android.os.Bundle
+import com.salomao.movies.R
 
 class MainActivity : AppCompatActivity() {
     override fun onCreate(savedInstanceState: Bundle?) {
