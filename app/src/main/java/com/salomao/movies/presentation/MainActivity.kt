d95edd3c47767521567a340147dc489b0825e6db
Rodrigo <rsalomao@paypal.com>
2022-04-19 22:27:31 -0300

    Enabling data binding and navigation

--- app/src/main/java/com/salomao/movies/presentation/MainActivity.kt
+++ app/src/main/java/com/salomao/movies/presentation/MainActivity.kt
@@ -1,12 +1,32 @@
 package com.salomao.movies.presentation
 
-import androidx.appcompat.app.AppCompatActivity
 import android.os.Bundle
+import androidx.appcompat.app.AppCompatActivity
+import androidx.navigation.NavController
+import androidx.navigation.findNavController
+import androidx.navigation.ui.AppBarConfiguration
+import androidx.navigation.ui.navigateUp
+import androidx.navigation.ui.setupActionBarWithNavController
 import com.salomao.movies.R
 
 class MainActivity : AppCompatActivity() {
+    private lateinit var appBarConfiguration: AppBarConfiguration
+    private lateinit var navController: NavController
+
     override fun onCreate(savedInstanceState: Bundle?) {
         super.onCreate(savedInstanceState)
         setContentView(R.layout.activity_main)
+        setNavigation()
+    }
+
+    private fun setNavigation() {
+        navController = findNavController(R.id.fragment_container)
+        navController.setGraph(R.navigation.main_nav_graph)
+        appBarConfiguration = AppBarConfiguration(navController.graph)
+        setupActionBarWithNavController(navController)
+    }
+
+    override fun onSupportNavigateUp(): Boolean {
+        return navController.navigateUp(appBarConfiguration) || super.onSupportNavigateUp()
     }
 }
--- app/src/main/java/com/salomao/movies/presentation/MovieListFragment.kt
+++ app/src/main/java/com/salomao/movies/presentation/MovieListFragment.kt
@@ -5,14 +5,31 @@ import android.view.LayoutInflater
 import android.view.View
 import android.view.ViewGroup
 import androidx.fragment.app.Fragment
+import androidx.navigation.fragment.findNavController
 import com.salomao.movies.R
+import com.salomao.movies.databinding.FragmentMovieListBinding
 
 class MovieListFragment : Fragment() {
+    private var _binding: FragmentMovieListBinding? = null
+    private val binding get() = _binding!!
 
     override fun onCreateView(
         inflater: LayoutInflater, container: ViewGroup?,
         savedInstanceState: Bundle?
-    ): View? {
-        return inflater.inflate(R.layout.fragment_movie_list, container, false)
+    ): View {
+        _binding = FragmentMovieListBinding.inflate(inflater)
+        return binding.root
+    }
+
+    override fun onViewCreated(view: View, savedInstanceState: Bundle?) {
+        super.onViewCreated(view, savedInstanceState)
+        binding.tvTitle.setOnClickListener {
+            findNavController().navigate(R.id.action_movieListFragment_to_movieDetailsFragment)
+        }
+    }
+
+    override fun onDestroy() {
+        super.onDestroy()
+        _binding = null
     }
 }
