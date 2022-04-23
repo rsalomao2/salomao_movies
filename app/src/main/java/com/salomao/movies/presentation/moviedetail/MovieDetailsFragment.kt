32f392a0268cdb116d9ba2edf26d05edc613d3bd
Rodrigo <rsalomao@paypal.com>
2022-04-23 13:17:08 -0300

    removing detail screen

--- app/src/main/java/com/salomao/movies/presentation/moviedetail/MovieDetailsFragment.kt
+++ app/src/main/java/com/salomao/movies/presentation/moviedetail/MovieDetailsFragment.kt
@@ -6,35 +6,17 @@ import android.view.View
 import android.view.ViewGroup
 import androidx.fragment.app.Fragment
 import com.salomao.movies.databinding.FragmentMovieDetailsBinding
-import com.salomao.movies.domain.di.injectMovieDetailKoin
-import com.salomao.movies.presentation.movielist.MovieListFragment.Companion.ARGS_MOVIE_ID
-import org.koin.android.viewmodel.ext.android.viewModel
 
 class MovieDetailsFragment : Fragment() {
     private var _binding: FragmentMovieDetailsBinding? = null
     private val binding get() = _binding!!
-    private val viewModel by viewModel<MovieDetailViewModel>()
 
     override fun onCreateView(
         inflater: LayoutInflater, container: ViewGroup?,
         savedInstanceState: Bundle?
     ): View {
-        injectMovieDetailKoin()
         _binding = FragmentMovieDetailsBinding.inflate(inflater)
         return binding.root
-    }
-
-    override fun onViewCreated(view: View, savedInstanceState: Bundle?) {
-        super.onViewCreated(view, savedInstanceState)
-        setupObservers()
-        val movieClicked = arguments?.getInt(ARGS_MOVIE_ID)
-        viewModel.getMovie(movieClicked)
-    }
-
-    private fun setupObservers() {
-        viewModel.movieDetailLiveData.observe(viewLifecycleOwner) { clickedMovie ->
-            binding.tvTitle.text = clickedMovie.name
-        }
     }
 
     override fun onDestroy() {
