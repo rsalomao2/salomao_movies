80b93dffde0eae123384aacc19a4f6eeac124842
Rodrigo Salomao <rodrigo.salomao@solvis.com.br>
2022-11-15 14:40:38 -0300

    Add Analytics

--- app/src/main/java/com/salomao/movies/presentation/MainActivity.kt
+++ app/src/main/java/com/salomao/movies/presentation/MainActivity.kt
@@ -1,10 +1,15 @@
 package com.salomao.movies.presentation
 
+import android.os.Build
 import android.os.Bundle
 import androidx.appcompat.app.AppCompatActivity
 import androidx.navigation.NavController
 import androidx.navigation.fragment.NavHostFragment
 import androidx.navigation.ui.setupActionBarWithNavController
+import com.microsoft.appcenter.AppCenter
+import com.microsoft.appcenter.analytics.Analytics
+import com.microsoft.appcenter.crashes.Crashes
+import com.salomao.movies.BuildConfig
 import com.salomao.movies.R
 
 
@@ -15,11 +20,19 @@ class MainActivity : AppCompatActivity() {
         super.onCreate(savedInstanceState)
         setContentView(R.layout.activity_main)
         setNavigation()
+        setupCI()
     }
 
     override fun onBackPressed() {
         super.onBackPressed()
         navController.popBackStack()
+    }
+
+    private fun setupCI() {
+        AppCenter.start(
+            application, BuildConfig.APP_CENTER_KEY,
+            Analytics::class.java, Crashes::class.java
+        )
     }
 
     private fun setNavigation() {
